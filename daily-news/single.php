1d2eb2e8742c128140e583d855ebdf2e31066597
Taku-Omino <47404544+Taku-Omino@users.noreply.github.com>
2023-05-27 16:39:16 +0900

    2023/05/27

--- daily-news/single.php
+++ daily-news/single.php
@@ -1,9 +1,32 @@
 <?php get_header(); ?>
+
 <main>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <!-- ループ内の単一投稿表示 -->
+    <div class="post">
+        <?php if(has_post_thumbnail()) : ?>
+        <div class="post-thumb">
+            <?php the_post_thumbnail(); ?>
+        </div>
+        <?php endif; ?>
+
+        <div class="post-content">
+            <h1><?php the_title(); ?></h1>
+            <p><?php the_content(); ?></p>
+
+            <div class="post-links">
+                <!-- 掲載記事 -->
+                <!-- ここにリンクを挿入する -->
+            </div>
+
+            <div class="post-share">
+                <button class="facebook-share">Facebookシェア</button>
+                <button class="twitter-share">Twitterシェア</button>
+            </div>
+        </div>
+    </div>
     <?php endwhile; else : ?>
-        <!-- 投稿がない場合の表示 -->
+    <p>投稿が見つかりません。</p>
     <?php endif; ?>
 </main>
+
 <?php get_footer(); ?>
