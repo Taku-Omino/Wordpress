9506b5641e3a50716f1cb193f850b138e2a1fe2f
Taku-Omino <47404544+Taku-Omino@users.noreply.github.com>
2023-06-05 21:02:06 +0900

    2023/06/05

--- daily-news/index.php
+++ daily-news/index.php
@@ -1,9 +1,20 @@
 <?php get_header(); ?>
+
 <main>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <!-- ループ内の投稿表示 -->
+        <div class="post">
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <div class="post-meta">
+                <span class="post-date"><?php the_date(); ?></span>
+                <span class="post-category"><?php the_category(', '); ?></span>
+            </div>
+            <div class="post-excerpt">
+                <?php the_excerpt(); ?>
+            </div>
+        </div>
     <?php endwhile; else : ?>
-        <!-- 投稿がない場合の表示 -->
+        <p>投稿が見つかりません。</p>
     <?php endif; ?>
 </main>
+
 <?php get_footer(); ?>
